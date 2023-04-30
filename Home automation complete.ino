01574ee3e434b80ae201a8c8d9c7107c32bce4b9
Coding World <105975964+Codingworld-org@users.noreply.github.com>
2023-04-30 11:29:05 +0530

    Updated code
    few changes in code

--- Home automation complete.ino
+++ Home automation complete.ino
@@ -1,17 +1,9 @@
 #include <Servo.h>
 #define ledPin 7
-#define servoPin 9
-#define fan 10
-Servo myservo;
 int state = 0;
-int pos1 = 0 ;
-int pos2 = 180;
 
 void setup() {
-  
-  myservo.attach(servoPin);
   pinMode(ledPin, OUTPUT);
-  pinMode(fan, OUTPUT);
   digitalWrite(ledPin, LOW);
   Serial.begin(9600);
 }
@@ -28,13 +20,4 @@ void loop() {
   digitalWrite(ledPin, HIGH);
   state = 0;
  } 
- else if (state == '2') {
-  myservo.write(pos1);
-  state = 0;                            
-  } 
- else if (state == '3') {
-  myservo.write(pos2);                                
-  state = 0;
- }
 }
-
